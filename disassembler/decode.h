8a1f397685c0fe22db178162579e94a8b87f0944
KyleMiles <krm504@nyu.edu>
2021-06-25 13:09:40 -0400

    Manual Autoformatter Fixes

--- disassembler/decode.h
+++ disassembler/decode.h
@@ -5,8 +5,9 @@
 #include <stdint.h>
 #include <string.h>
 
-#include "encodings_dec.h"
 #include "operations.h"
+
+#include "encodings_dec.h"
 #include "regs.h"
 #include "sysregs.h"
 
@@ -144,7 +145,7 @@ enum ArrangementSpec
 	-3  // spec says this space is unallocated, eg: UNALLOCATED_10_branch_reg
 #define DECODE_STATUS_UNDEFINED \
 	-4  // spec says this encoding is undefined, often due to a disallowed field
-      // or a missing feature, eg: "if !HaveBF16Ext() then UNDEFINED;"
+	    // or a missing feature, eg: "if !HaveBF16Ext() then UNDEFINED;"
 #define DECODE_STATUS_END_OF_INSTRUCTION \
 	-5  // spec decode EndOfInstruction(), instruction executes as NOP
 #define DECODE_STATUS_LOST          -6  // descended past a checks, ie: "SEE encoding_up_higher"
