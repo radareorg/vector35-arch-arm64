951efd08e24dfb7a9d91226e5628b0a626acac15
Brian Potchik <brian@vector35.com>
2022-11-13 20:09:21 -0500

    Fix R_AARCH64_PREL64 relocation warning.

--- arch_arm64.cpp
+++ arch_arm64.cpp
@@ -3097,8 +3097,7 @@ class Arm64ElfRelocationHandler : public RelocationHandler
 		return true;
 	}
 
-	virtual bool GetRelocationInfo(
-	    Ref<BinaryView> view, Ref<Architecture> arch, vector<BNRelocationInfo>& result) override
+	virtual bool GetRelocationInfo(Ref<BinaryView> view, Ref<Architecture> arch, vector<BNRelocationInfo>& result) override
 	{
 		(void)view;
 		(void)arch;
@@ -3149,6 +3148,10 @@ class Arm64ElfRelocationHandler : public RelocationHandler
 				reloc.pcRelative = true;
 				reloc.size = 4;
 				break;
+			case R_AARCH64_PREL64:
+				reloc.pcRelative = true;
+				reloc.size = 8;
+				break;
 			case R_AARCH64_MOVW_UABS_G0:
 			case R_AARCH64_MOVW_UABS_G0_NC:
 			case R_AARCH64_MOVW_UABS_G1:
@@ -3189,8 +3192,7 @@ class Arm64ElfRelocationHandler : public RelocationHandler
 			}
 		}
 		for (auto& reloc : relocTypes)
-			LogWarn("Unsupported ELF relocation type: %s",
-			    GetRelocationString((ElfArm64RelocationType)reloc));
+			LogWarn("Unsupported ELF relocation type: %s", GetRelocationString((ElfArm64RelocationType)reloc));
 		return true;
 	}
 
